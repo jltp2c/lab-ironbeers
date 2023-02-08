7e5d6144076414eddf41644256e2289ba00fbeb2
Jean-Luc TAN <jeanluc.tan75@gmail.com>
2023-02-08 17:12:59 +0100

    commit 1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const hbs = require('hbs');
+const async = require('hbs/lib/async');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -13,13 +14,42 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(`${__dirname}/views/partials`);
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Home Beers',
+    navbar: true
+  });
+});
+app.get('/beers', async (req, res, next) => {
+  const displayBeerImg = await punkAPI.getBeers();
+  console.log(displayBeerImg);
+  try {
+    res.render('beers', {
+      allBeer: displayBeerImg,
+      title: 'Beers',
+      navbar: true
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+app.get('/random-beer', async (req, res, next) => {
+  const displayRandomImg = await punkAPI.getRandom();
+
+  try {
+    res.render('random-beer', {
+      beerRandom: displayRandomImg,
+      title: 'Random Beer',
+      navbar: true
+    });
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
